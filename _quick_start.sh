953c7b4e077dc840c17b4e03c0d48bc58d0dcd2c
KikaXiaoYu <shinoxiaoyu@outlook.com>
2024-08-21 14:49:20 +0800

    update xzaq

---
+++ _quick_start.sh
@@ -0,0 +1,4 @@
+cd D:/Entertainment/DeathBellTowering/Story_teller_xiaoyu/townsquare-main
+npm run serve
+
+# localhost:8080
--- self_README.txt
+++
@@ -1,7 +0,0 @@
-d:
-
-cd D:\Entertainment\DeathBellTowering\说书人指南专项\townsquare-main
-
-npm run serve
-
-localhost:8080
